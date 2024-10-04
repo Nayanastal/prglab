a45a29caa5077c6413f552a4ebfce97bb992df8e
Nayanastal <9listal@gmail.com>
2024-10-04 12:11:25 +0530

    Add files via upload

---
+++ positive_04-10-2024.py
@@ -0,0 +1,15 @@
+i=int(input("enter a number"))
+if(i>0):
+    {
+         print(i,"is positive")
+    }
+
+elif(i<0):
+    {
+        print(i,"is negative")
+    }
+else:
+    {
+        print("zero")
+    }
+   
