4f3e3c4f45ffb14cc726908d6707d1294dc45270
Nayanastal <9listal@gmail.com>
2024-10-04 11:53:46 +0530

    Add files via upload

---
+++ area_27-09-2024.py
@@ -0,0 +1,13 @@
+r=int(input("enter the radius"))
+a=3.14*r*r
+print(a)
+
+l=int(input("enter the length"))
+b=int(input("enter the breadth"))
+c=l*b
+print(c)
+
+b1=int(input("enter the breadth"))
+h=int(input("enter the height"))
+d=1/2*b1*h
+print(d)
