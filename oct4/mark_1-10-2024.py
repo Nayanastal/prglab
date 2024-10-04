65ab38b502d196bda22d0e664bdc790e8013edaf
Nayanastal <9listal@gmail.com>
2024-10-04 13:09:46 +0530

    Add files via upload

---
+++ oct4/mark_1-10-2024.py
@@ -0,0 +1,10 @@
+print("mark of 5 subjects")
+a=float (input("enter the marks"))
+b=float (input("enter the marks"))
+c=float (input("enter the marks"))
+d=float (input("enter the marks"))
+e=float (input("enter the marks"))
+total=a+b+c+d+e
+print("total marks=",total)
+percentage=total/50*100
+print("percentage of marks=",percentage)
