5a27f378bbdacfb0eaea3dafe73de4807481ffe1
Nayanastal <9listal@gmail.com>
2024-10-04 11:58:37 +0530

    Add files via upload

---
+++ oct4/sum_27-09-2024.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=a+b
+print(c)
+
+d=a-b
+print(d)
+
+e=a*b
+print(e)
+
+f=a/b
+print(f)
---
+++ oct4/swap_01-10-2024.py
@@ -0,0 +1,7 @@
+a=int(input("enter a number"))
+b=int(input("enter another number"))
+print("before swapping=",a,b)
+a=a+b
+b=a-b
+a=a-b
+print("after swapping=",a,b)
