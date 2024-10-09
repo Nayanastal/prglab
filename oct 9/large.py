09d66d2bb7b7999e869f5b7fbc533a1f069af733
Nayanastal <9listal@gmail.com>
2024-10-09 11:21:52 +0530

    Update large.py

--- oct 9/large.py
+++ oct 9/large.py
@@ -1,9 +1,9 @@
 a=int(input("Enter first number :"))
 b=int(input("Enter second number :"))
 c=int(input("Enter third number :"))
-if a>b:
+if a>b and a>c:
     print("a is greater",a)
-elif b>c:
+elif b>a and b>c:
     print("b is greater",b)
 else:
     print("c is greater",c)
