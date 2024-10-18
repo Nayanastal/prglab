aaee2bde53feafb61a7531dfdf859e84b62449e9
Nayanastal <9listal@gmail.com>
2024-10-18 16:23:02 +0530

    Add files via upload

---
+++ oct18/occurenceofa.py
@@ -0,0 +1,8 @@
+
+list=input("Enter the First name : ")
+Name=list.split()
+print("list is",Name)
+count=0
+for name in Name:
+    count+=name.lower().count('a')
+print("occurence of a is : ",count)
