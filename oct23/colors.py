287c928e394934572256c3dea051a14ccd10f953
Nayanastal <9listal@gmail.com>
2024-10-23 12:11:33 +0530

    Add files via upload

---
+++ oct23/colors.py
@@ -0,0 +1,6 @@
+colors=[]
+count=int(input("Enter the size :"))
+for i in range(count):
+         colors.append(input("color :"))
+print("List of colors are:",colors )
+print("First and last colour are ",[colors[0],colors[-1]])
