25b67ef1d0db6a0d3bdd3f3c517eeb51174abad5
Nayanastal <9listal@gmail.com>
2024-10-23 12:32:18 +0530

    Add files via upload

---
+++ oct23/maxmin.py
@@ -0,0 +1,13 @@
+list=input("enter a list")
+List=[]
+for n in list:
+    List.append(int(n))
+print("The list is \n",List)
+print("minimum value from the list",min(List))
+print("maximum value from the list",max(List))
+print("length of the list",len(List))
+print("sum of the list",sum(List))
+sort=sorted(List)
+print("sort the list",sort)
+sort.reverse()
+print("reverse of the list",sort)
