136dce18d374b9244506a5599b2328a358c32201
Nayanastal <9listal@gmail.com>
2024-10-30 15:51:51 +0530

    Update numberpyramid.py

--- oct30/numberpyramid.py
+++ oct30/numberpyramid.py
@@ -1,14 +1,13 @@
-# Set the number of rows for the pyramid
-number_of_rows = 5
+number_of_rows =int(input("Enter a number:"))
 
 for i in range(1, number_of_rows + 1):
-    # Print leading spaces for formatting
-    print(" " * (number_of_rows - i), end="")
+    
+    
     
     for j in range(1, i + 1):
-        # Calculate the product and print it
+        
         product = i * j
         print(product, end=" ")
     
-    # Move to the next line after each row
+
     print()
